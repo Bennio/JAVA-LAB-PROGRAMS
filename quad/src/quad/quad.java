73f372c9e163a6c9cb2685b840d2506df494f183
Mrinal <waliamrinal@gmail.com>
2018-11-13 01:16:41 +0530

    Solving Quadratic equation program written in eclipse java language

---
+++ quad/src/quad/quad.java
@@ -0,0 +1,35 @@
+package quad;
+import java.util.*;
+
+public class quad {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		double r1,r2,d;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter coeff a, b, c");
+		a= s.nextInt();
+		b= s.nextInt();
+		c= s.nextInt();
+		
+		d = (b*b)-4*a*c;
+		
+		if(d>=0)
+		{
+			System.out.println("Roots are real and positive");
+			r1 = ( -b + Math.sqrt(d) ) / (2*a);
+			r2 = ( -b - Math.sqrt(d) ) / (2*a);
+			System.out.println("First root is:"+r1);
+			System.out.println("Second root is:"+r2); 
+		}
+		
+		else {
+			System.out.println("Roots are complex");
+			r1 = -b / (2*a);
+			r2 = Math.sqrt(-d) / (2*a);
+			System.out.println("Root 1 = "+r1+"+i"+r2);
+			System.out.println("Root 1 = "+r1+"-i"+r2);
+		}
+	}
+
+}
