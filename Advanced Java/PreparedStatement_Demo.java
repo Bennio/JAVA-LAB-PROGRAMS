0a39a8afc67a4e2b95d37c0a9a731120dbb17dda
abhiwalia15 <waliamrinal@gmail.com>
2019-10-13 17:11:25 +0530

    prepared statements in jdbc

---
+++ Advanced Java/PreparedStatement_Demo.java
@@ -0,0 +1,81 @@
+package delete_this;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+ 
+public class PreparedStatement_Demo {
+    public static void main(String[] argv) {
+ 
+        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found !!");
+            return;
+        }
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+        try {
+            connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/Object_Detection", "root", "");
+            
+            String query = " Select * from images where serial_no = ? OR serial_no = ? ";
+         PreparedStatement pst = (PreparedStatement) connection.prepareStatement(query);
+         
+        // pst.setString(1, "images");
+         pst.setInt(1, 20);
+         
+         
+         
+         pst.setInt(2,10);
+         
+            ResultSet rs = pst.executeQuery();
+            //ResultSetMetaData rsmd = rs.getMetaData();
+            
+//            char  hi = rs.toString();
+            
+            System.out.println("\nSQL Connection to database established!");
+            while(rs.next())
+            {
+        	   //System.out.println("hello");
+
+        	   System.out.println("\n");
+        	   //System.out.println(rsmd.getColumnName(1) + " " + rsmd.getColumnName(2));
+        	   System.out.print(rs.getInt(1)+" | " +rs.getString(2));
+        	   
+        	   
+        	   
+           }
+           
+            
+            System.out.println("\n");System.out.println("\n");
+           
+            
+            
+            
+            
+            
+ 
+        } catch (SQLException e) {
+            //System.out.println("Connection Failed! Check output console");
+            return;
+        } finally {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+                System.out.println("Connection closed !!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
